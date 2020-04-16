080d396e70e98a36b601fc9cfc5f72753e32fcfa
unknown <omicalpha3@gmail.com>
2020-04-17 00:39:12 +0430

    html pages linked / index handled separately

--- dist/index.js
+++ dist/index.js
@@ -11,24 +11,49 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 _http["default"].createServer(function (req, res) {
   var query = _url["default"].parse(req.url, true);
 
-  var filename = "__dirname/../src/html".concat(query.pathname);
-  console.log(' CURRENT PATH :', __dirname);
+  console.log(' CURRENT URL :', req.url);
+  var filename;
 
-  _fs["default"].readFile(filename, function (error, data) {
-    if (error) {
-      console.log('ERROR~!', error, data, filename);
-      res.writeHead(404, {
+  if (req.url === '/') {
+    console.log('MA INjAYIM');
+    filename = "__dirname/../src/frontend/html/index.html";
+
+    _fs["default"].readFile(filename, function (error, data) {
+      if (error) {
+        console.log('ERROR~!', error, data, filename);
+        res.writeHead(404, {
+          'Content-Type': 'text/html'
+        });
+        return res.end("error ".concat(filename));
+      }
+
+      res.writeHead(200, {
         'Content-Type': 'text/html'
       });
-      return res.end("error ".concat(filename));
-    }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
+      res.write(data);
+      return res.end();
     });
-    res.write(data);
-    return res.end();
-  }); //   fs.readFile(filename, (err, data) => {
+  } else {
+    filename = "__dirname/../src/frontend/html".concat(query.pathname, ".html");
+
+    _fs["default"].readFile(filename, function (error, data) {
+      if (error) {
+        console.log('ERROR~!', error, data, filename);
+        res.writeHead(404, {
+          'Content-Type': 'text/html'
+        });
+        return res.end("error ".concat(filename));
+      }
+
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.write(data);
+      return res.end();
+    });
+  }
+
+  console.log(' CURRENT PATH :', __dirname); //   fs.readFile(filename, (err, data) => {
   //     if (err) {
   //       res.writeHead(404, { 'Content-Type': 'text/html' });
   //       return res.end(`404 Page Not Found ${filename}`);
@@ -37,7 +62,6 @@ _http["default"].createServer(function (req, res) {
   //     res.write(data);
   //     return res.end();
   //   });
-
 }).listen(8080);
 
 console.log('server running !');
--- src/server/index.js
+++ src/server/index.js
@@ -5,28 +5,34 @@ import fs from 'fs';
 http
   .createServer((req, res) => {
     const query = url.parse(req.url, true);
+    console.log(' CURRENT URL :', req.url);
 
-    const filename = `__dirname/../src/html${query.pathname}`;
-    console.log(' CURRENT PATH :', __dirname);
-    fs.readFile(filename, (error, data) => {
-      if (error) {
-        console.log('ERROR~!', error, data, filename);
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        return res.end(`error ${filename}`);
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    });
-    //   fs.readFile(filename, (err, data) => {
-    //     if (err) {
-    //       res.writeHead(404, { 'Content-Type': 'text/html' });
-    //       return res.end(`404 Page Not Found ${filename}`);
-    //     }
-    //     res.writeHead(200, { 'Content-Type': 'text/html' });
-    //     res.write(data);
-    //     return res.end();
-    //   });
+    let filename;
+    if (req.url === '/') {
+      filename = `__dirname/../src/frontend/html/index.html`;
+      fs.readFile(filename, (error, data) => {
+        if (error) {
+          console.log('ERROR~!', error, data, filename);
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end(`error ${filename}`);
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      filename = `__dirname/../src/frontend/html${query.pathname}.html`;
+      fs.readFile(filename, (error, data) => {
+        if (error) {
+          console.log('ERROR~!', error, data, filename);
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end(`error ${filename}`);
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    }
   })
   .listen(8080);
 
